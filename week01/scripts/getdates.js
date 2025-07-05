93623161a5be61ef4161b36bddb43ca2fb4f18da
karenmahr <kmahr@byupathway.edu>
2025-07-05 19:13:55 -0300

    1st project submit

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,9 @@
+<head>
+    ...
+    <script src="scripts/getdates.js" defer>
+        const currentyear = document.querySelector("#year");
+        year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+    </script>
+
+    alert(document.lastModified);
+</head>
