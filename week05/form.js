d2c3b9ef5fc93754d69696f8f0042e13291f73c9
karenmahr <kmahr@byupathway.edu>
2025-08-05 22:23:23 -0300

    html y css done, still have to write javascript code

---
+++ week05/form.js
@@ -0,0 +1,34 @@
+const today = new Date();
+const currentYearSpan = document.querySelector("#currentyear");
+currentYearSpan.textContent = today.getFullYear();
+
+const lastMofified = document.querySelector("#lastModified");
+lastModified.textContent = `Last modified: ${document.lastModified}`;
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
